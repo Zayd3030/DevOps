99948c34e86d3906a5b91a9cf968634b65bb9800
“Zayd3030” <mzaydh@gmail.com>
2024-12-12 15:21:37 +0000

    corrected error in server.js file

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ var requests=0;
 var podname= process.env.HOSTNAME;
 var startTime;
 var host;
+
 var handleRequest = function(request, response) {
  response.setHeader('Content-Type', 'text/plain');
  response.writeHead(200);
  response.write("DevOps Coursework 2! | Running on: ");
  response.write(host);
  response.end(" | v=0\n");
- console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App 
-Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());}
+ console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());}
+
 var www = http.createServer(handleRequest);
 www.listen(8080,function () {
  startTime = new Date();;
